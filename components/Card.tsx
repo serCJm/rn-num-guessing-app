2a2161fc046fb538370ebf86c363017478fc1889
cjmoro <scjmoro@gmail.com>
2020-07-28 18:18:15 -0700

    Add Card component

---
+++ components/Card.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, View, ViewStyle } from "react-native";
+
+type Props = {
+	style: ViewStyle;
+};
+
+const Card: React.FC<Props> = ({ children, style }) => {
+	return <View style={{ ...styles.card, ...style }}>{children}</View>;
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+	card: {
+		shadowColor: "black",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowRadius: 6,
+		shadowOpacity: 0.26,
+		elevation: 5,
+		backgroundColor: "white",
+		padding: 20,
+		borderRadius: 10,
+	},
+});
