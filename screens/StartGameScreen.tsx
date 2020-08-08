12b3d93991ac275af7d4bde9966e6cd278294ea9
cjmoro <scjmoro@gmail.com>
2020-08-08 16:07:26 -0700

    Add flexible width

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -121,8 +121,11 @@ const styles = StyleSheet.create({
 		fontFamily: "open-sans-bold",
 	},
 	inputContainer: {
-		width: 300,
-		maxWidth: "80%",
+		// width: 300,
+		// maxWidth: "80%",
+		width: "80%",
+		minWidth: 300,
+		maxWidth: "95%",
 		alignItems: "center",
 	},
 	buttonContainer: {
